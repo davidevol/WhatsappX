636e10503180644a237bfad776581c9591c0de0a
deeh <deeh@tuta.io>
2021-11-09 10:57:25 -0300

    Adicionando as dependências do Firebase.

--- app/src/main/java/com/davidev/whatsappx/MainActivity.java
+++ app/src/main/java/com/davidev/whatsappx/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Dependências do Firebase
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtem a instância do Firebase.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
     }
 }
