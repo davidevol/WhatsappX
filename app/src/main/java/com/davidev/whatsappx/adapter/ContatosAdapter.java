720e13ebd9ae6d08342326d318d9a4ebefa03f2d
Davidev <deehzi@pm.me>
2022-01-15 12:48:55 -0300

    Os usuários aparecem com perfil em tempo real na aba Contatos.

---
+++ app/src/main/java/com/davidev/whatsappx/adapter/ContatosAdapter.java
@@ -0,0 +1,76 @@
+package com.davidev.whatsappx.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.davidev.whatsappx.R;
+import com.davidev.whatsappx.model.Usuario;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+
+    private List<Usuario> contatos;
+    private Context context;
+
+    public ContatosAdapter(List<Usuario> listaContatos, Context c) {
+        this.contatos = listaContatos;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemLista = LayoutInflater.from(parent.getContext() ).inflate(R.layout.adapter_contatos, parent, false);
+
+        return new MyViewHolder( itemLista );
+    }
+
+    @Override
+    // Recupera o email, nome e uri da foto
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Usuario usuario = contatos.get(position);
+
+        holder.nome.setText( usuario.getNome() );
+        holder.email.setText( usuario.getEmail() );
+
+        if( usuario.getFoto() != null ){
+            Uri uri = Uri.parse( usuario.getFoto() );
+            Glide.with(context).load( uri ).into( holder.foto );
+        }else {
+            holder.foto.setImageResource( R.drawable.padrao );
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatos.size();
+    }
+
+    public  class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome, email;
+
+        public  MyViewHolder(View itemView){
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.imageViewFotoContato);
+            nome = itemView.findViewById(R.id.textNomeContato);
+            email = itemView.findViewById(R.id.textEmailContato);
+        }
+    }
+}
--- app/src/main/java/com/davidev/whatsappx/fragment/ContatosFragment.java
+++ app/src/main/java/com/davidev/whatsappx/fragment/ContatosFragment.java
@@ -2,17 +2,34 @@ package com.davidev.whatsappx.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.davidev.whatsappx.R;
+import com.davidev.whatsappx.adapter.ContatosAdapter;
+import com.davidev.whatsappx.config.ConfiguracaoFirebase;
+import com.davidev.whatsappx.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class ContatosFragment extends Fragment {
 
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatatos = new ArrayList<>();
+    private DatabaseReference usuariosRef;
+    private ValueEventListener valueEventListenerContatos;
 
     public ContatosFragment() {
     }
@@ -26,6 +43,57 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        // Configurações Iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.reciclerViewListaContatos);
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
+        // Adapter
+        adapter = new ContatosAdapter(listaContatatos, getActivity());
+
+        // Configuração do recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getActivity() );
+        recyclerViewListaContatos.setLayoutManager( layoutManager );
+        recyclerViewListaContatos.setHasFixedSize( true );
+        recyclerViewListaContatos.setAdapter(adapter);
+
+         return view;
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        recuperarContatos();
+    }
+
+    public void onStop(){
+        super.onStop();
+        usuariosRef.removeEventListener( valueEventListenerContatos );
+    }
+
+    // Adiciona ao adapter os valores novos
+    public void recuperarContatos(){
+
+        valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dados: snapshot.getChildren() ){
+
+                    Usuario usuario = dados.getValue( Usuario.class );
+                    listaContatatos.add(usuario);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
